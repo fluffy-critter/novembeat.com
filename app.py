2a3bead7e5f213926b73dd67261167457c6281d3
fluffy <fluffy@beesbuzz.biz>
2021-10-16 22:34:22 -0700

    Add player generation endpoint and start of submission system

--- app.py
+++ app.py
@@ -2,12 +2,22 @@
 
 import logging
 import logging.handlers
-import os
+import os, os.path
+import email.message
+import urllib.parse
 
+import arrow
 from flask_hookserver import Hooks
 import flask
 import publ
 import authl.flask
+import publ.user, publ.entry
+from publ.caching import cache
+import slugify
+import atomicwrites
+from pony import orm
+import requests
+from bs4 import BeautifulSoup
 
 if os.path.isfile('logging.conf'):
     logging.config.fileConfig('logging.conf')
@@ -16,7 +26,7 @@ else:
         os.makedirs('logs')
     except FileExistsError:
         pass
-    logging.basicConfig(level=logging.INFO,
+    logging.basicConfig(level=logging.DEBUG,
                         handlers=[
                             logging.handlers.TimedRotatingFileHandler(
                                 'logs/publ.log', when='D'),
@@ -104,3 +114,129 @@ def deploy(data, delivery):
     threading.Timer(3, restart_server, args=[os.getpid()]).start()
 
     return flask.Response(result, mimetype='text/plain')
+
+def parse_url(url):
+    parsed = urllib.parse.urlparse(url)
+    if not parsed.scheme:
+        parsed = parsed._replace(scheme, 'https')
+    return parsed
+
+@app.route('/_preview')
+def generate_preview():
+    return generate_iframe(flask.request.args['url'])
+
+@cache.memoize()
+def generate_iframe(url):
+    parsed = parse_url(url)
+    url = parsed.geturl()
+
+    LOGGER.debug("Parsed URL: %s", parsed)
+    if parsed.netloc.lower().endswith('youtube.com'):
+        # YouTube supports opengraph but it strips out playlists :(
+        qs = urllib.parse.parse_qs(parsed.query)
+        LOGGER.debug("Parsed query string: %s", qs)
+        if 'list' in qs:
+            return f'''<iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?list={qs['list'][0]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'''
+        if 'v' in qs:
+            return f'''<iframe width="100%" height="315" src="https://www.youtube.com/embed/{qs['v'][0]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'''
+        raise http_error.BadRequest("Missing playlist or video ID")
+
+    req = requests.get(parsed.geturl())
+    if req.status_code != 200:
+        flask.abort(req.code)
+
+    if 'audio/' in req.headers['content-type']:
+        return f'''<audio src="{url}" type="{req.headers['content-type']}" controls>'''
+    if 'video/' in req.headers['content-type']:
+        return f'''<video src="{url}" type="{req.headers['content-type']}" controls>'''
+
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    def find_opengraph(*tags):
+        for og_tag in tags:
+            node = soup.find('meta', {'property':og_tag, 'content':True})
+            if node:
+                return node['content']
+
+        return None
+
+    vidurl = find_opengraph('og:video:secure_url', 'og:video', 'twitter:player')
+    if vidurl:
+        width = find_opengraph('og:video:width', 'twitter:player:width') or '100%'
+        height = find_opengraph('og:video:height', 'twitter:player:height') or '150'
+        return f'<iframe src="{vidurl}" width="{width}" height="{height}" allow="accelerometer; autoplay; picture-in-picture"></iframe>'
+
+    raise http_error.UnsupportedMediaType(f"Don't know how to handle URL {url}")
+
+
+def get_entry_text(form):
+
+    parsed = parse_url(form['entry-url'])
+    text = f'''
+[Playlist]({parsed.geturl()}):
+
+{generate_iframe(parsed)}
+'''
+
+    if 'comment' in form:
+        text += f'''
+.....
+
+{form['comment']}
+'''
+
+
+@app.route('/_submit', methods=['POST'])
+def submit_entry():
+    user = publ.user.get_active()
+    if not user:
+        raise http_error.Unauthorized()
+    if 'banned' in user.groups:
+        raise http_error.Forbidden()
+
+    form = flask.request.form
+    output = email.message.Message()
+
+    output['Title'] = form['author-name']
+    output['Author'] = form['author-name']
+    if 'author-url' in form:
+        output['Author-URL'] = form['author-url']
+
+    output['submitter'] = user.identity
+
+    if 'admin' not in user.groups:
+        output['auth'] = user.identity
+
+    date = arrow.get()
+    year = int(form['year'])
+    if year < 2016:
+        raise http_error.BadRequest(f"Novembeat wasn't happening in {year}")
+    elif year < now.year:
+        date = now.replace(year=year)
+    elif year > now.year:
+        raise http_error.BadRequest(f'{year} is in the future')
+    output['Date'] = date.format('YYYY-MM-DD')
+
+    output.set_content(get_entry_text(form['entry-url', form.get('comment')]))
+
+    authorname = slugify.slugify(form['author-name'])
+    filename = os.path.join('content', 'works', f'submission-{year} {authorname}.md')
+    fullpath = os.path.join(APP_PATH, filename)
+
+    # See https://github.com/PlaidWeb/Publ/issues/471 for a proposed better way to do this
+    try:
+        with atomicwrites.atomic_write(fullpath) as file:
+            file.write(str(output))
+    except FileExistsError:
+        raise http_error.Conflict(f"{filename}: file exists")
+
+    for fixup in range(5):
+        if entry.scan_file(fullpath, filename, fixup):
+            break
+
+    with orm.db_session():
+        import publ.model
+        entry_record = publ.model.Entry.get(file_path=filename)
+        entry_obj = publ.entry.Entry.load(entry_record)
+        return flask.redirect(entry_obj.archive(paging='year'))
+
