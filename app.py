1cd164bcaea978595bcd793d670eafa6a1fba632
fluffy <fluffy@beesbuzz.biz>
2022-11-01 19:55:19 -0700

    Unpack the preview HTML

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def generate_preview():
     try:
         return f'''
         <!-- $url -->
-        { generate_iframe(parse_url(flask.request.args['url']))}
+        { generate_iframe(parse_url(flask.request.args['url']))[0] }
         '''
     except http_error.HTTPException as error:
         return str(error), error.code
