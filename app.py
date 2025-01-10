09dd0d67a791d98cb653ed7bdc30846b31d72433
fluffy <fluffy@beesbuzz.biz>
2025-01-09 17:23:59 -0800

    Add ProxyFIx wrapper for hypercorn

--- app.py
+++ app.py
@@ -409,3 +409,5 @@ Filename: {entry_obj.file_path}
     send_func(msg)
     LOGGER.info("Sent?")
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
